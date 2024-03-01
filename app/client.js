dc9d646110a71b37d872fba73f35ad3045f4e412
NaranjaDEV <1109920456@u.icesi.edu.co>
2024-02-29 23:28:11 -0500

    Refactor getUser and sendUser functions

--- app/client.js
+++ app/client.js
@@ -1,35 +1,38 @@
-async function getUser(){
-    const query = await fetch('http://localhost:3003/user')
-    const data = await query.json()
-    return(data)
+async function getUser() {
+  const query = await fetch("http://localhost:3003/user");
+  const data = await query.json();
+  localStorage.setItem("user", JSON.stringify(data));
+  const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
 }
+;
+const btn = document.getElementById("btn-sign");
 
-const btn = document.getElementById("btn-sign")
+const inputs = document.getElementsByClassName("sign-up");
 
-const inputs = document.getElementsByClassName("sign-up")
 
 let users = {
-    user: "",
-    pass: ""
-}
+  user: "",
+  pass: "",
+};
 
-async function sendUser(){
-    const box = {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify(users)
-    }
-    fetch ("http://localhost:3003/user", box)
+async function sendUser() {
+  const box = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(users),
+  };
+  fetch("http://localhost:3003/user", box);
 }
 btn.addEventListener("click", async (e) => {
-    e.preventDefault()
-    users = {
-        user: inputs[0].value,
-        pass: inputs[1].value,
-    }
-    console.log(users)
-    await sendUser()
-        window.location.href = '/app/screens/loan.html'; 
-})
+  e.preventDefault();
+  users = {
+    user: inputs[0].value,
+    pass: inputs[1].value,
+  };
+  console.log(users);
+  await sendUser();
+  window.location.href = "/app/screens/loan.html";
+});
