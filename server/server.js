996a30ba4aa37d51073dbe534cda37858d7b574b
NaranjaDEV <1109920456@u.icesi.edu.co>
2024-02-29 22:02:51 -0500

    Refactor server.js file to add initial book data and update routes

--- server/server.js
+++ server/server.js
@@ -5,28 +5,23 @@ const PORT = 3030;
 app.use(express.json());
 app.use("/", express.static("app"));
 
-let books = [];
-
-async function showbooks() {
-    const query = await fetch(`https://stephen-king-api.onrender.com/api/books`);
-    const data = await query.json();
-    books.push(data);
-    console.log(books);
-}
-
-// Call the showbooks function
-showbooks();
-
+let books = [
+  {
+    Title: "The Great Gatsby",
+    id: 1,
+  },
+];
 
 app.get("/books", (req, res) => {
-  res.send(books)});
+  res.send(books);
+});
 
-  app.post("/books", (request, response) => {
-    console.log(request.body);
-    books.push(request.body);
-    response.end();
-  });
+app.post("/books", (request, response) => {
+  console.log(request.body);
+  books.push(request.body);
+  response.end();
+});
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}/app`)
-})
+  console.log(`http://localhost:${PORT}/app`);
+});
