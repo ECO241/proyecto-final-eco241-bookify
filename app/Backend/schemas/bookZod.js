6aaa122ddeddaa90dc788d9598743756e48a55bc
Soleil <solangegarciaguamanga@gmail.com>
2024-04-28 00:12:57 -0500

    scaffolding?

--- app/Backend/schemas/bookZod.js
+++
@@ -1,9 +0,0 @@
-const z = require('zod')
-
-const bookschema = z.object({
-    nombre: z.string(),
-    autor: z.string(),
-    edicion: z.string(),
-});
-
-module.exports = bookschema
--- app/Backend/schemas/userZod.js
+++
@@ -1,8 +0,0 @@
-const z = require('zod')
-
-const userschema = z.object({
-    nombre: z.string(),
-    codigo: z.string(),
-});
-
-module.exports = userschema
